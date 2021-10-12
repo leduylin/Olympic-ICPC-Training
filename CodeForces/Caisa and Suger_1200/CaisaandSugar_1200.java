2f2ec28cc6a2f69a8d74af48d9a22f66edbd21b2
leduylin <lin.le.set18@eiu.edu.vn>
2021-10-12 20:30:41 +0700

    Create CaisaandSugar_1200.java
    caisa and sugar done

---
+++ CodeForces/Caisa and Suger_1200/CaisaandSugar_1200.java
@@ -0,0 +1,130 @@
+import java.io.*;
+import java.util.*;
+
+public class CaisaandSugar_1200 {
+
+	public static void main(String[] args) {
+		int n = ni(), s = ni();
+		int maxSweets = 0;
+		boolean canBuy = false;
+		while (n-- > 0) {
+			int x = ni(), y = ni();
+			double moneyNeeded = (double) x + (double) y / 100;
+			int numSugarBought = (int) ((double) s / moneyNeeded);
+			if (numSugarBought > 0) {
+				canBuy = true;
+				int numSweets = 0;
+				if (y > 0)
+					numSweets = 100 - y;
+				maxSweets = Math.max(maxSweets, numSweets);
+			}
+		}
+		if (!canBuy)
+			maxSweets = -1;
+		System.out.println(maxSweets);
+	}
+
+	static InputStream is = System.in;
+
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static float nf() {
+		return Float.parseFloat(ns());
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+}
