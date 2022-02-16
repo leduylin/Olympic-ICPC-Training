88d7154ce034949b35f6fb5b298ed82be560e801
leduylin <lin.le.set18@eiu.edu.vn>
2022-02-16 09:10:15 +0700

    AC Exponentiation II

---
+++ CSES/Exponentiation2.java
@@ -0,0 +1,128 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Exponentiation2 {
+
+	public static void main(String[] args) {
+		int t = ni();
+		StringBuilder outBuffer = new StringBuilder();
+		while (t-- > 0) {
+			long a = nl();
+			long b = nl();
+			long c = nl();
+			int M = 1000000007;
+			long res = binaryExpoenttiation(a, binaryExpoenttiation(b, c, M - 1), M);
+			outBuffer.append(res + "\n");
+		}
+		System.out.println(outBuffer);
+	}
+
+	static int binaryExpoenttiation(long a, long b, int M) {
+		if (b == 0)
+			return 1;
+		long res = binaryExpoenttiation(a, b / 2, M);
+		if (b % 2 == 1)
+			return (int) (((res * res) % (M) * a) % M);
+		return (int) ((res * res) % M);
+	}
+
+	static InputStream is = System.in;
+	static byte[] inbuf = new byte[1 << 24];
+	static int lenbuf = 0, ptrbuf = 0;
+
+	static int readByte() {
+		if (lenbuf == -1)
+			throw new InputMismatchException();
+		if (ptrbuf >= lenbuf) {
+			ptrbuf = 0;
+			try {
+				lenbuf = is.read(inbuf);
+			} catch (IOException e) {
+				throw new InputMismatchException();
+			}
+			if (lenbuf <= 0)
+				return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+
+	static boolean isSpaceChar(int c) {
+		return !(c >= 33 && c <= 126);
+	}
+
+	static int skip() {
+		int b;
+		while ((b = readByte()) != -1 && isSpaceChar(b))
+			;
+		return b;
+	}
+
+	static double nd() {
+		return Double.parseDouble(ns());
+	}
+
+	static char nc() {
+		return (char) skip();
+	}
+
+	static String ns() {
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while (!(isSpaceChar(b))) {
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+
+	static char[] ns(int n) {
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while (p < n && !(isSpaceChar(b))) {
+			buf[p++] = (char) b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	static int ni() {
+		int num = 0, b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	static long nl() {
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+			;
+		if (b == '-') {
+			minus = true;
+			b = readByte();
+		}
+		while (true) {
+			if (b >= '0' && b <= '9') {
+				num = num * 10 + (b - '0');
+			} else {
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+}
