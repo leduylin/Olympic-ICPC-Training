53cbd70511e139272f4ce5015d52a03158c00047
leduylin <lin.le.set18@eiu.edu.vn>
2022-04-09 00:02:25 +0700

    array in C++

--- C++/1. Data Structures & STL Containers/Array in C++/main.cpp
+++ C++/1. Data Structures & STL Containers/Array in C++/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+void updateArray(int arr[], int i, int val){
+    arr[i] = val;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int arr[5] = {1,5,4,3,2};
+    int n = sizeof(arr)/sizeof(int);
+
+    updateArray(arr, 1, 13);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
